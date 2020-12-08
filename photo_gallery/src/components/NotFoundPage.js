90efab3601c260c306d7b096b37ade410d2a2b64
Piyush Jasaiwal <coderpiyush123@gmail.com>
2020-12-08 08:52:48 +0530

    empty

--- photo_gallery/src/components/NotFoundPage.js
+++ photo_gallery/src/components/NotFoundPage.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const NotFoundPage = () => {
-    return(
-        <div>
-            <h3>Page Not Found</h3>
-        </div>
-    );
-};
+const NotFoundPage = () => (
+    <div>
+      <h3>Page not found</h3>
+    </div>
+);
 
-export dafault NotFoundPage;
+export default NotFoundPage;
